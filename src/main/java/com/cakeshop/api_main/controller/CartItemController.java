63d4be8445e021e71a87784fc13b51b6a8185804
NIX <121375847+loikimtrang@users.noreply.github.com>
2025-04-14 14:43:27 +0700

    add order api

--- src/main/java/com/cakeshop/api_main/controller/CartItemController.java
+++ src/main/java/com/cakeshop/api_main/controller/CartItemController.java
@@ -1,12 +1,16 @@
 package com.cakeshop.api_main.controller;
 
+import com.cakeshop.api_main.dto.request.cartItem.UpdateCartItemRequest;
 import com.cakeshop.api_main.dto.response.BaseResponse;
 import com.cakeshop.api_main.exception.ErrorCode;
 import com.cakeshop.api_main.exception.NotFoundException;
-import com.cakeshop.api_main.repository.internal.*;
+import com.cakeshop.api_main.model.CartItem;
+import com.cakeshop.api_main.repository.internal.ICartItemRepository;
+import com.cakeshop.api_main.repository.internal.ICartRepository;
 import com.cakeshop.api_main.utils.BaseResponseUtils;
 import com.cakeshop.api_main.utils.SecurityUtil;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -23,6 +27,21 @@ import org.springframework.web.bind.annotation.*;
 public class CartItemController {
     ICartRepository cartRepository;
     ICartItemRepository cartItemRepository;
+
+    @PutMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE)
+    public BaseResponse<Void> update(
+            @Valid @RequestBody UpdateCartItemRequest request
+    ) {
+        CartItem cartItem = cartItemRepository.findById(request.getCartItemId())
+                .orElseThrow(() -> new NotFoundException("ITEM_NOT_FOUND_IN_CART", ErrorCode.RESOURCE_NOT_EXISTED));
+        if (request.getQuantity() == 0) {
+            cartItemRepository.deleteById(request.getCartItemId());
+        } else {
+            cartItem.setQuantity(request.getQuantity());
+            cartItemRepository.save(cartItem);
+        }
+        return BaseResponseUtils.success(null, "Updated cart item successfully");
+    }
 
     @DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public BaseResponse<Void> delete(@PathVariable String id) {
