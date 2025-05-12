6749efc99c062419f41101ccf878aa5a9c8edee4
huypham <huyphuocbinh204@gmail.com>
2025-05-12 14:46:46 +0700

    add data category and sort price product

--- src/main/java/com/cakeshop/api_main/model/criteria/ProductCriteria.java
+++ src/main/java/com/cakeshop/api_main/model/criteria/ProductCriteria.java
@@ -1,12 +1,10 @@
 package com.cakeshop.api_main.model.criteria;
 
 import com.cakeshop.api_main.constant.BaseConstant;
+import com.cakeshop.api_main.model.Discount;
 import com.cakeshop.api_main.model.Product;
 import com.cakeshop.api_main.model.criteria.base.BaseCriteria;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -51,10 +49,32 @@ public class ProductCriteria extends BaseCriteria<Product> {
                 predicates.add(cb.lessThanOrEqualTo(root.get("price"), getToPrice()));
             }
             if (StringUtils.hasText(getPriceSort())) {
+                Join<Product, Discount> discountJoin = root.join("discount", JoinType.LEFT);
+                Path<Integer> discountRaw = discountJoin.get("discountPercentage");
+
+                Expression<Integer> discountInverse = cb.diff(
+                        cb.literal(100),
+                        discountRaw
+                );
+
+                Expression<Double> discountFactor = cb.prod(
+                        cb.toDouble(discountInverse),
+                        cb.literal(0.01)
+                );
+
+                Expression<Double> discountMultiplier = cb.<Double>selectCase()
+                        .when(cb.isNotNull(discountRaw), discountFactor)
+                        .otherwise(cb.literal(1.0));
+
+                Expression<Double> priceAfterDiscount = cb.prod(
+                        root.get("price"),
+                        discountMultiplier
+                );
+
                 if (getPriceSort().equalsIgnoreCase(BaseConstant.SORT_ASC)) {
-                    query.orderBy(cb.asc(root.get("price")));
+                    query.orderBy(cb.asc(priceAfterDiscount));
                 } else if (getPriceSort().equalsIgnoreCase(BaseConstant.SORT_DESC)) {
-                    query.orderBy(cb.desc(root.get("price")));
+                    query.orderBy(cb.desc(priceAfterDiscount));
                 }
             }
             if (StringUtils.hasText(getCreatedSort())) {
